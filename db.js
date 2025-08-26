4c10ac490093f5c0b33d3d90f3ec6f2387b009c1
mohibr33 <riazmohib@gmail.com>
2025-08-26 20:57:20 +0500

    Initial commit

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
-const mysql = require("mysql2");
-const dotenv = require("dotenv");
+import mysql from "mysql2";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -8,15 +8,15 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: 3306
+  port: process.env.DB_PORT // 3306
 });
 
 db.connect((err) => {
   if (err) {
-    console.error(" MySQL connection failed:", err.message);
+    console.error("Database connection failed:", err.stack);
     return;
   }
-  console.log(" Connected to MySQL ");
+  console.log("Connected to MySQL database ✅");
 });
 
-module.exports = db;
+export default db;
